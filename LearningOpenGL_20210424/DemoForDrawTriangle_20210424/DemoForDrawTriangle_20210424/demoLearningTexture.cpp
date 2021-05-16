8ddfd70bad94049283ce30c89616835de26962fd
linxiaoyi <lyh15@meitu.com>
2021-05-17 01:23:52 +0800

    【Feature】Learning texture1

--- LearningOpenGL_20210424/DemoForDrawTriangle_20210424/DemoForDrawTriangle_20210424/demoLearningTexture.cpp
+++ LearningOpenGL_20210424/DemoForDrawTriangle_20210424/DemoForDrawTriangle_20210424/demoLearningTexture.cpp
@@ -39,17 +39,18 @@ int main(){
     unsigned int VBO;
     unsigned int EBO;
     unsigned int VAO;
-    unsigned int texture;
+    unsigned int texture1, texture2;
     
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
     glGenVertexArrays(1, &VAO);
-    glGenTextures(1, &texture);
+    glGenTextures(1, &texture1);
+    
     
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBindTexture(GL_TEXTURE_2D,texture);
+    glBindTexture(GL_TEXTURE_2D,texture1);
     
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(index),index,GL_STATIC_DRAW);
@@ -60,38 +61,68 @@ int main(){
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 
     int width, height, nrChannels;
-    
-    char const *filename ="./resources/container.jpeg";
-    unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned char* data = stbi_load("./resources/container.jpeg", &width, &height, &nrChannels, 0);
     if(data){
         glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }else
     {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "Failed to load texture1" << std::endl;
     }
     stbi_image_free(data);
     
+    
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    data = stbi_load("./resources/awesomeface.png", &width, &height, &nrChannels, 0);
+    if(data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else{
+        std::cout << "Failed to load texture2." <<std::endl;
+    }
+    stbi_image_free(data);
+
+
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 8*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
-    
+
     glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE, 8*sizeof(float),(void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
-    
+
     glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,8*sizeof(float),(void*)(6*sizeof(float)));
     glEnableVertexAttribArray(2);
-    
+
     glBindBuffer(GL_ARRAY_BUFFER,0);
-    glBindTexture(GL_TEXTURE_2D,0);
+
     
+
     xiaoyi::myShaderProgram shaderProgram("./vs/demoLearningTexture.vs","./fs/demoLearningTexture.fs");
+    shaderProgram.use();
+    shaderProgram.setInt("ourTexture1", 0);
+    shaderProgram.setInt("ourTexture2", 1);
+    
     while (!glfwWindowShouldClose(myGlfw::getInstance()->mWindow)) {
         myGlfw::getInstance()->processInput(myGlfw::getInstance()->mWindow);
         glClearColor(0.2f,0.3f,0.3f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        
         shaderProgram.use();
-        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
         glfwSwapBuffers(myGlfw::getInstance()->mWindow);
--- LearningOpenGL_20210424/DemoForDrawTriangle_20210424/DemoForDrawTriangle_20210424/fs/demoLearningTexture.fs
+++ LearningOpenGL_20210424/DemoForDrawTriangle_20210424/DemoForDrawTriangle_20210424/fs/demoLearningTexture.fs
@@ -3,9 +3,13 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D ourTexture1;
+uniform sampler2D ourTexture2;
+
 
 void main()
 {
-    FragColor = texture(ourTexture,TexCoord) * vec4(ourColor,1.0f);
+//    FragColor = texture(ourTexture1,TexCoord) * vec4(ourColor,1.0f);
+    FragColor = mix(texture(ourTexture1,TexCoord),texture(ourTexture2,TexCoord),0.2);
 }
+                
